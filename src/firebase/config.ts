4f83ca103e6e36b8ebb6ba40e72e404b7766e289
Jesse Santos <jesse@nexaya.com.br>
2026-03-05 02:29:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1364489068-73ecf",
+  "appId": "1:731738903584:web:2d9526fc6ee72a3dc6d845",
+  "apiKey": "AIzaSyCGziJBhgdcyN1dJpCpsjUR7sG1aigDmZc",
+  "authDomain": "studio-1364489068-73ecf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "731738903584"
+};
